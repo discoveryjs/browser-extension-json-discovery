49f446cd553f28226c4f7638d36fcdc504705139
Denis Kolesnikov <exsdis@gmail.com>
2019-08-02 23:57:08 +0300

    Refactored style namespacing

--- core/cssTransformLoader.js
+++ core/cssTransformLoader.js
@@ -2,16 +2,19 @@ const csstree = require('css-tree');
 const { WRAPPER_NODE } = require('./constants');
 
 const walkTree = ast => {
-    csstree.walk(ast, function(node) {
-        if (node.type === 'Selector') {
-            node.children.prepend(node.children.createItem({
-                type: 'WhiteSpace',
-                value: ' '
-            }));
-            node.children.prepend(node.children.createItem({
-                type: 'ClassSelector',
-                name: WRAPPER_NODE
-            }));
+    csstree.walk(ast, {
+        visit: 'Rule',
+        enter: function(node) {
+            node.prelude.children.forEach(child => {
+                child.children.prepend(child.children.createItem({
+                    type: 'WhiteSpace',
+                    value: ' '
+                }));
+                child.children.prepend(child.children.createItem({
+                    type: 'ClassSelector',
+                    name: WRAPPER_NODE
+                }));
+            });
         }
     });
 
