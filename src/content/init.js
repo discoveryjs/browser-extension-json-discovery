ee2e7a5023822e5a930bd80b4557d64948d4f73f
Denis Kolesnikov <exsdis@gmail.com>
2020-11-06 23:21:39 +0100

    Fixed lint

--- src/content/init.js
+++ src/content/init.js
@@ -61,7 +61,7 @@ export function init(getSettings) {
                     discoveryNode,
                     raw: textContent,
                     data: json,
-                    settings,
+                    settings
                 });
             });
         }
