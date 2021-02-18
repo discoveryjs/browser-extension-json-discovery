7fa3870d4044f9d0fd0707f03bd18e8baaf01434
Roman Dvornov <rdvornov@gmail.com>
2021-02-18 17:45:42 +0100

    Basic implementation for loading progress tracking and chunk parsing

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,7 +63,7 @@ module.exports = {
         'no-compare-neg-zero': 2,
         'no-cond-assign': 2,
         'no-console': 2,
-        'no-constant-condition': 2,
+        'no-constant-condition': 0,
         'no-control-regex': 2,
         'no-debugger': 2,
         'no-dupe-args': 2,
--- src/content/init.js
+++ src/content/init.js
@@ -1,18 +1,101 @@
 import { preloader as createPreloader } from '@discoveryjs/discovery/dist/discovery-preloader.js';
+import { parseChunked } from '@discoveryjs/json-ext';
 import { initDiscovery } from './init-discovery';
 
 let loaded = document.readyState === 'complete';
+let loadedTimer;
 let pre = null;
+let preCursor;
 let initialPreDisplay = null;
-let af;
 let preloader;
+let pushChunk = () => {};
+const chunkBuffer = [];
+const getChunk = () => {
+    if (chunkBuffer.length) {
+        return chunkBuffer.shift();
+    }
 
-af = requestAnimationFrame(async function x() {
+    return new Promise(resolve => {
+        pushChunk = chunk => {
+            resolve(chunk);
+            pushChunk = chunk => chunkBuffer.push(chunk);
+        };
+    });
+};
+const data = parseChunked(async function*() {
+    const loadStartTime = Date.now();
+    let size = 0;
+    const getState = done => ({
+        stage: 'receive',
+        progress: {
+            done,
+            elapsed: Date.now() - loadStartTime,
+            units: 'bytes',
+            completed: size
+        }
+    });
+
+    while (true) {
+        const chunk = await getChunk();
+
+        if (!chunk) {
+            break;
+        }
+
+        yield chunk;
+        size += chunk.length;
+
+        if (preloader) {
+            await preloader.progressbar.setState(getState(false));
+        }
+    }
+
+    preloader.progressbar.setState(getState(true));
+});
+
+data.catch(error => {
+    cancelAnimationFrame(loadedTimer);
+    preloader.el.remove();
+    document.body.style.cssText = '';
+
+    if (pre !== null) {
+        requestAnimationFrame(() => {
+            // it might to take a lot of time to render large text,
+            // so make it visible in next frame to allow styles rollback
+            pre.style.display = initialPreDisplay;
+            pre = null;
+        });
+    }
+
+    console.error('[JsonDiscovery] Failed to parse JSON', error); // eslint-disable-line no-console
+});
+
+const flushData = () => {
+    if (pre === null) {
+        return;
+    }
+
+    while (true) {
+        const chunkNode = preCursor === undefined ? pre.firstChild : preCursor.nextSibling;
+
+        if (!chunkNode) {
+            break;
+        }
+
+        if (chunkNode.nodeType === Node.TEXT_NODE) {
+            pushChunk(chunkNode.nodeValue);
+        }
+
+        preCursor = chunkNode;
+    }
+};
+
+loadedTimer = requestAnimationFrame(async function checkLoaded() {
     if (
+        initialPreDisplay === null &&
         document.body &&
         document.body.firstElementChild &&
-        document.body.firstElementChild.tagName === 'PRE' &&
-        initialPreDisplay === null
+        document.body.firstElementChild.tagName === 'PRE'
     ) {
         pre = document.body.firstElementChild;
         initialPreDisplay = window.getComputedStyle(pre).display;
@@ -24,32 +107,25 @@ af = requestAnimationFrame(async function x() {
             styles: [{ type: 'link', href: chrome.runtime.getURL('loader.css') }],
             darkmode: settings.darkmode
         });
+        preloader.progressbar.setState({ stage: 'request' });
     }
 
     if (!loaded) {
-        cancelAnimationFrame(af);
-        af = requestAnimationFrame(x);
+        flushData();
+        loadedTimer = requestAnimationFrame(checkLoaded);
+        return;
     }
 
-    if (pre !== null && loaded) {
+    if (pre !== null) {
         let json;
 
-        const textContent = pre.textContent.trim();
-
-        if (!textContent.startsWith('{') && !textContent.startsWith('[')) {
-            pre.style.display = initialPreDisplay;
-            return;
-        }
+        flushData();
+        pushChunk(null);
 
         try {
-            json = JSON.parse(textContent);
+            json = await data;
+            preloader.progressbar.setState({ stage: 'done' });
         } catch (e) {
-            pre.style.display = initialPreDisplay;
-            console.error(e.message); // eslint-disable-line no-console
-        }
-
-        if (!json) {
-            pre.style.display = initialPreDisplay;
             return;
         }
 
@@ -58,14 +134,13 @@ af = requestAnimationFrame(async function x() {
 
         document.body.style.margin = 0;
         document.body.style.padding = 0;
-        document.body.style.height = '100vh';
         document.body.style.border = 'none';
 
         const settings = await getSettings();
 
         await initDiscovery({
             node: document.body,
-            raw: textContent,
+            raw: pre.textContent, // FIXME: should be computed lazyly
             settings,
             styles: [chrome.runtime.getURL('index.css')],
             progressbar: preloader.progressbar
